5e47b4e5040ea72dd072a5bf0ca80efa3089a3e2
p1-kawamura <kawamura@p1-intl.com>
2022-08-01 10:50:38 +0900

    com1050

--- dx_it/houjin/views.py
+++ dx_it/houjin/views.py
@@ -75,7 +75,6 @@ def left(request):
     if toroku_to != "":
         str["toroku__lte"]=toroku_to
     
-    print(tantou)
 
     #検索データ取得
     if tantou=="99":
@@ -201,88 +200,88 @@ def download(request):
 
 
 def upload(request):
-    #顧客一覧
-    data = io.TextIOWrapper(request.FILES['csv1'].file, encoding="cp932")
-    csv_content = csv.reader(data)
+    # #顧客一覧
+    # data = io.TextIOWrapper(request.FILES['csv1'].file, encoding="cp932")
+    # csv_content = csv.reader(data)
     
-    csv_list=list(csv_content)
+    # csv_list=list(csv_content)
         
-    h=0
-    for i in csv_list:
-        if h!=0:
-            Customer.objects.update_or_create(
-                cus_id=i[0],
-                defaults={
-                    "cus_id":i[0],
-                    "sei":i[1],
-                    "mei":i[2],
-                    "sei_kana":i[3],
-                    "mei_kana":i[4],
-                    "mail1":i[5],
-                    "mail2":i[6],
-                    "mail3":i[7],
-                    "yubin":i[8],
-                    "pref":i[9],
-                    "city":i[10],
-                    "banchi":i[11],
-                    "build":i[12],
-                    "com":i[13],
-                    "busho":i[14],
-                    "tel":i[15],
-                    "mob":i[16],
-                    "fax":i[17],
-                    "toroku":i[18],
-                    "kensu":i[19],
-                    "money":i[20],
-                    "adress":i[21],
-                }            
-            )
-        h+=1   
+    # h=0
+    # for i in csv_list:
+    #     if h!=0:
+    #         Customer.objects.update_or_create(
+    #             cus_id=i[0],
+    #             defaults={
+    #                 "cus_id":i[0],
+    #                 "sei":i[1],
+    #                 "mei":i[2],
+    #                 "sei_kana":i[3],
+    #                 "mei_kana":i[4],
+    #                 "mail1":i[5],
+    #                 "mail2":i[6],
+    #                 "mail3":i[7],
+    #                 "yubin":i[8],
+    #                 "pref":i[9],
+    #                 "city":i[10],
+    #                 "banchi":i[11],
+    #                 "build":i[12],
+    #                 "com":i[13],
+    #                 "busho":i[14],
+    #                 "tel":i[15],
+    #                 "mob":i[16],
+    #                 "fax":i[17],
+    #                 "toroku":i[18],
+    #                 "kensu":i[19],
+    #                 "money":i[20],
+    #                 "adress":i[21],
+    #             }            
+    #         )
+    #     h+=1   
 
 
-    #見積受注
-    data = io.TextIOWrapper(request.FILES['csv2'].file, encoding="cp932")
-    csv_content = csv.reader(data)
+    # #見積受注
+    # data = io.TextIOWrapper(request.FILES['csv2'].file, encoding="cp932")
+    # csv_content = csv.reader(data)
     
-    csv_list=list(csv_content)
+    # csv_list=list(csv_content)
         
-    h=0
-    for i in csv_list:
-        if h!=0:
-            Recieve.objects.update_or_create(
-                rec_id=i[0],               
-                defaults={
-                    "rec_id":i[0],
-                    "rec_no":i[1],
-                    "rec_ver":i[2],
-                    "status":i[3],
-                    "mitsu_day":i[4],
-                    "rec_day":i[5],
-                    "eigyou_sei":i[6],
-                    "eigyou_mei":i[7],
-                    "eigyou_busho":i[8],
-                    "rec_cus_id":Customer.objects.get(cus_id=i[9]),
-                    "keiro":i[10],
-                    "mitsu_money":i[11],
-                }                
-            )
-        h+=1
+    # h=0
+    # for i in csv_list:
+    #     if h!=0:
+    #         Recieve.objects.update_or_create(
+    #             rec_id=i[0],               
+    #             defaults={
+    #                 "rec_id":i[0],
+    #                 "rec_no":i[1],
+    #                 "rec_ver":i[2],
+    #                 "status":i[3],
+    #                 "mitsu_day":i[4],
+    #                 "rec_day":i[5],
+    #                 "eigyou_sei":i[6],
+    #                 "eigyou_mei":i[7],
+    #                 "eigyou_busho":i[8],
+    #                 "rec_cus_id":Customer.objects.get(cus_id=i[9]),
+    #                 "keiro":i[10],
+    #                 "mitsu_money":i[11],
+    #             }                
+    #         )
+    #     h+=1
 
 
-    #見積商品
-    data = io.TextIOWrapper(request.FILES['csv3'].file, encoding="cp932")
-    csv_content = csv.reader(data)
+    # #見積商品
+    # data = io.TextIOWrapper(request.FILES['csv3'].file, encoding="cp932")
+    # csv_content = csv.reader(data)
     
-    csv_list=list(csv_content)
+    # csv_list=list(csv_content)
         
-    h=0
-    for i in csv_list:
-        if h!=0:
-            Item.objects.create(
-                item_rec_id=Recieve.objects.get(rec_id=i[0]),
-                item_name=i[1],           
-            )
-        h+=1
+    # h=0
+    # for i in csv_list:
+    #     if h!=0:
+    #         Item.objects.create(
+    #             item_rec_id=Recieve.objects.get(rec_id=i[0]),
+    #             item_name=i[1],           
+    #         )
+    #     h+=1
 
 
 
@@ -291,16 +290,17 @@ def upload(request):
     csv_content = csv.reader(data)
     
     csv_list=list(csv_content)
-        
-    for i in csv_list:
-        Customer.objects.update_or_create(
-            cus_id=i[0],
-            defaults={
-                "cus_id":i[0],
-                "kensu":i[1],
-                "money":i[2],
-            }            
-        )  
+
+    if len(csv_list[0])>0:
+        for i in csv_list:
+            Customer.objects.update_or_create(
+                cus_id=i[0],
+                defaults={
+                    "cus_id":i[0],
+                    "kensu":i[1],
+                    "money":i[2],
+                }            
+            )  
 
     return redirect("houjin:index")
 
